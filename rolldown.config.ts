8b7d428254162c701c261e673aab36aef4b0f046
ParasSolanki <parassolanki151@gmail.com>
2025-03-06 21:59:22 +0530

    wip: idk this will work

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -8,13 +8,6 @@ export default defineConfig({
   output: {
     file: "dist/app.js",
   },
-  define: {
-    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-    "process.env.JIRA_PERSONAL_ACCESS_TOKEN": JSON.stringify(
-      process.env.JIRA_PERSONAL_ACCESS_TOKEN,
-    ),
-    "process.env.JIRA_BASE_URL": JSON.stringify(process.env.JIRA_BASE_URL),
-  },
   plugins: [
     minify({
       module: true,
