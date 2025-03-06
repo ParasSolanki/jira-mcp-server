875475cecb5b4c67c16e4f5ae47caa79c19399b4
ParasSolanki <parassolanki151@gmail.com>
2025-03-06 21:22:56 +0530

    wip: changeset

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 import { getUserAgent } from "universal-user-agent";
+import pkg from "../package.json" with { type: "json" };
 
-export const VERSION = "0.0.1";
+export const VERSION = pkg.version;
 export const USER_AGENT = `modelcontextprotocol/servers/jira/v${VERSION} ${getUserAgent()}`;
